9534476a8bc3473f32b331785a52e8cb5eab8ea5
Willian Hercilio Pezzini <williantijucas@gmail.com>
2024-09-09 05:38:07 -0300

    Alterando responsividade para as imagens não vazarem do container, trocado cor de texto do botão para carregar mais Pokemons, Alterado quantidade limite de pokemons para 386 contemplando até a 3º geração. Alterado quantidade de cars por linha conforme o tamanho ta tela.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,8 @@
 const pokemonList = document.getElementById('pokemonList')
 const loadMoreButton = document.getElementById('loadMoreButton')
 
-const maxRecords = 151
-const limit = 10
+const maxRecords = 386
+const limit = 12
 let offset = 0;
 
 function convertPokemonToLi(pokemon) {
